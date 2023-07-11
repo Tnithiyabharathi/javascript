5aa48ce6b71790bf3749e70460f48cc81be61a7b
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-11 15:18:42 +0530

    using add,sub,multiply,modulus,increment,decrement

---
+++ calculator.js
@@ -0,0 +1,39 @@
+// addition
+ let a = 3;
+ let b = 4;
+ let addition = a + b;
+ console.log(addition); // 7
+
+ //subtraction
+ let x = 5;
+ let y = 3;
+ let subtraction = x - y; 
+ console.log(subtraction);  //2
+
+ //multiply
+ let num1 = 5;
+ let num2 = 3;
+ let multiply = num1 * num2;
+ console.log(multiply); //15
+
+ //divition 
+let num3 = 10;
+let num4 = 5;
+let divition = num3 / num4;
+console.log(divition); // 2
+
+//modulus 
+let m = 25;
+let n = 2;
+let modulus = m % n;
+console.log(modulus); // 1
+
+//increment 
+let increment = 5;
+increment += 5;
+console.log(increment); //10
+
+//decrement
+let decrement = 10;
+decrement -= 5;
+console.log(decrement); //5
