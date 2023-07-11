f2114de0a1071d2e0cf349a6f3cb2768d5d13fea
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-11 16:00:43 +0530

    comparison operator

---
+++ comparison operator.js
@@ -0,0 +1,25 @@
+//equal to 
+
+const a= 4;
+console.log(a == "5"); //false
+
+
+//lessthan and greaterthan 
+
+let x = 5;
+let y = 6;
+console.log(x < y); //true
+console.log(x > y); //false
+
+
+//Math.min() and Math.max
+
+let m = 6;
+let n = 2;
+console.log(Math.min(m, n));
+// 2
+
+console.log(Math.max(m,n)); //6
+
+
+
