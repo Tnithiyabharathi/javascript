bf97060d3c3a96cd2d4df0623329d2f170c843f9
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-14 13:35:55 +0530

    added a nested function

---
+++ nested object.js
@@ -0,0 +1,18 @@
+const houseNames = {
+    firstRedHouse: {
+      Names: {
+        Girls: ["Priya", "Gayathri"],
+        Boys: ["Praveen", "Nivin"]
+      }
+    },
+    secondGreenHouse: {
+      Names: {
+        Girls: ["Kavya", "Sangeetha"],
+        Boys: ["Mohan", "Siva"]
+      }
+    }
+  };
+  
+  const firstHouseName = houseNames["firstRedHouse"].Names["Boys"];
+  console.log(firstHouseName);
+  
