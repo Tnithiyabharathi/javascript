373381392344c160a15466fd92b13a67df9fb776
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-14 14:30:15 +0530

    added a array

---
+++ nested array.js
@@ -0,0 +1,22 @@
+const houseNames = [
+    {
+     firstRedHouse : "StudentsName0",
+        Names: ["Priya", 
+        "Gayathri",
+        "Praveen", 
+        "Nivin"]
+    
+    },
+    {
+     secondGreenHouse : "StudentsName1",   
+        Names: 
+          ["Kavya",
+           "Sangeetha",
+           "Mohan",
+           "Siva"]
+      
+    }
+  ];
+  const firstHouseName = houseNames[1].Names[0];
+  console.log(firstHouseName);
+  
