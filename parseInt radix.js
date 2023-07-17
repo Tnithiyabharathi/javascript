5a831b33a6e1ab78b2cda3fcc0ab2aee0e650a39
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-17 13:32:23 +0530

    added a progams

---
+++ parseInt radix.js
@@ -0,0 +1,13 @@
+//parseInt
+function convertToInt(str){
+    return parseInt(str)
+}
+convertToInt("56");
+
+//parseInt radix
+
+function convertToInteger(str) {
+    return parseInt(str,2);
+    
+    }
+ convertToInteger("10011");
--- testing object.js
+++ testing object.js
@@ -1,14 +1,15 @@
-var checkObj = {
+var obj ={
   gift : "pony",
-  pet  : "kitten", 
-  bed  : "sleigh"
+  pet : "kitten",
+  bed : "slieigh"
 };
-  function checkObject(checkProp){
-     if(checkObj.hasOwnProperty(checkProp))
-     {
-         return checkObj[checkProp];
-     }else{
-         return "Not Found";
-     }
+function checkObj(obj,checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  }else {
+  return "Not Found";
+  }
+  // Only change code above this line
 };
-console.log(checkObject("gift"));  //pony
+console.log(checkObj("gift")); 
