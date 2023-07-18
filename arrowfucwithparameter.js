abfbf352db67e40b7f7e3456a183cfebf7d87ebd
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-18 16:13:03 +0530

    default parameters for our functions

--- arrowfucwithparameter.js
+++ arrowfucwithparameter.js
@@ -4,3 +4,8 @@
 
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+const increment = (number, value = 1) => number + value ;
+ console.log(increment(5));
+ 
