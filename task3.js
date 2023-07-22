4316275a6667439c05f7c0ca2164af1e565a2bf8
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-22 08:48:57 +0530

    added a program and print all the keys

---
+++ task3.js
@@ -0,0 +1,19 @@
+//write a function takes an object as argument and print all keys:
+
+function obj(){
+    let person = {
+      name : "Nithya",
+      age : 22,
+      favcolor : "Pink"
+    } 
+      for(let key in person)
+      {
+      console.log(key); 
+      }
+    }
+    obj();
+
+    //name
+    //age
+    //favcolor
+    
