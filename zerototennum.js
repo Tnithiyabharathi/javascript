c514ca1e151c7cdef0c3657f2260ca9f060a1816
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-22 07:01:27 +0530

    added a zero to ten numbers

---
+++ zerototennum.js
@@ -0,0 +1,11 @@
+function zeroToTen(num1,num2){
+    if(num2 < num1){
+        return [];
+    }else{
+        const number = zeroToTen(num1, num2 - 1);
+        number.push();
+        return number;
+    }
+}
+console.log(zeroToTen(0,10));
+//0,1,2,3,4,,5,6,7,8,9,10
