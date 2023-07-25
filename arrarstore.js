4eb042694c2b5da80580327b4bb140774e44355f
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-25 21:57:49 +0530

    added  apop,push,unshift,shift,short,filter

---
+++ arrarstore.js
@@ -0,0 +1,6 @@
+let yourArray = ["a", 2, true, null, {name : "nithya"}];
+
+//array content using bracket:
+
+let myArray = ["a", "nithya", "c", "d"]
+console.log(myArray);
---
+++ arraypushandunshift.js
@@ -0,0 +1,5 @@
+function mixedNumbers(arr){
+    arr.push("I", 2,"three");
+    arr.unshift(7, "VIII", 9);
+}
+console.log(mixedNumbers(["IV", 5, "six"]));
