bc4e32ea8e42dce912b325ef1a035738e0b1b78d
nithyabharathi <nithyathangappan06@gmail.com>
2023-07-26 07:21:51 +0530

    change code

--- mutation.js
+++ mutation.js
@@ -1,9 +1,10 @@
 function mutation(arr) {
     const test = arr[1].toLowerCase();
-    const target = arr[0].toUpperCase();
-    for(let i = 0;i < test.length; i++){
-      if(target.indexOf(test[1])< 0)
-      return false;
-    }return true;
+    const target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
   }
+  
   mutation(["hello", "hey"]);
